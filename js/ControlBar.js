10a1450cbe64b7b3f85fe63bd85ad7a55557569a
jfiser <jfiser42@gmail.com>
2016-07-12 16:08:30 -0400

    Enable user ability to filter results

--- js/ControlBar.js
+++ js/ControlBar.js
@@ -1,7 +1,19 @@
 function ControlBar(_main){
     this.main = _main;
+    this.filterTxt = "";
+    this.setListeners();
 }
 ControlBar.prototype.filterTxtClicked = function(){
-    console.log("zzz");
     this.filterTxt = ($("#filterTxt").val());
+}
+ControlBar.prototype.setListeners = function(){
+    var _self = this;
+    document.getElementById("filterTxt").addEventListener("keypress", function (evt) {
+        if (evt.keyCode === 13)   {
+            $("#filterSubmitBtn").click();
+            evt.preventDefault(); // <<< Most important missing piece from icedwater
+            _self.filterTxt = ($("#filterTxt").val());
+            console.log("filterTxt: " + _self.filterTxt);
+        }
+    });
 }
